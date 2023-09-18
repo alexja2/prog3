6752599a5f9ac57f237f11cd31afc07d9371572d
Alex Jauregui <Alex Jauregui@LAPTOP-Alex>
2023-09-18 17:56:20 +0200

    proyecto0

---
+++ ProyectoGranSlam/src/gs/Main.java
@@ -0,0 +1,25 @@
+package gs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		VentanaGrandSlam v = new VentanaGrandSlam();
+		v.setVisible(true);
+		
+		//VentanaClasificacion v2 = new VentanaClasificacion(v);
+		//v2.setVisible(true);
+		
+	
+
+		 
+
+	        
+	    
+	}
+
+}
